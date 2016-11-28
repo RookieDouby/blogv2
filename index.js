344725225f71978de44b774adc8a6f474e8cd658
RookieDouby <zhd1109@qq.com>
2016-11-29 02:11:23 +0800

    update configuration items

--- index.js
+++ index.js
@@ -1,7 +1,11 @@
+var path = require('path');
 var express = require('express');
 var app = express();
 var indexRouter = require('./routes/index');
 var userRouter = require('./routes/users');
+
+app.set('views',path.join(__dirname, 'views'));//set directory of tempalte files
+app.set('view engine', 'ejs');
 
 app.use('/',indexRouter);
 app.use('/users',userRouter);
--- routes/users.js
+++ routes/users.js
@@ -2,7 +2,9 @@ var express = require('express');
 var router = express.Router();
 
 router.get('/:name',function(req, res) {//name is a placeholder
-    res.send('Hello, ' + req.params.name);
+    res.render('users', {
+        name: req.params.name
+    });
 });
 
 module.exports = router;
